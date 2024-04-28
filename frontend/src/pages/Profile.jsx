189c15c6e44c13317cc84637d9daa19c5500470e
syeef <syeefislam@yahoo.com>
2024-04-28 10:38:10 +0530

    email verify done

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,13 +1,80 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import UpdateProfile from "./UpdateProfile";
+import axios from "axios";
+import Button from "react-bootstrap/Button";
 
 function Profile() {
+  const [emailVerified, setEmailVerfied] = useState(false);
+  const sendEmailVerification = async () => {
+    try {
+      const key = import.meta.env.VITE_FIREBASE_APP_ID;
+      const token = localStorage.getItem("token");
+
+      const response = await axios.post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=${key}`,
+        {
+          requestType: "VERIFY_EMAIL",
+          idToken: token,
+        }
+      );
+
+      if (response.status === 200) {
+        //console.log(response.data.users[0].displayName);
+        // if (
+        //   response.data.users[0].displayName &&
+        //   response.data.users[0].photoUrl
+        // ) {
+        //   setComplete(true);
+        // }
+        // fullnameRef.current.value = response.data.users[0].displayName;
+        // profile_photo_urlRef.current.value = response.data.users[0].photoUrl;
+        console.log("Email verification is send!");
+        //alert("User details fetched!");
+      }
+    } catch (error) {
+      console.log(error);
+      //alert(error.response.data.error.message);
+    }
+  };
+
+  const checkEmailVerification = async () => {
+    try {
+      const key = import.meta.env.VITE_FIREBASE_APP_ID;
+      const token = localStorage.getItem("token");
+
+      const response = await axios.post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:update?key=${key}`,
+        {
+          requestType: "VERIFY_EMAIL",
+          idToken: token,
+        }
+      );
+
+      if (response.status === 200) {
+        // console.log(response);
+        console.log("email", response.data.emailVerified);
+        if (response.data.emailVerified === true) {
+          setEmailVerfied(true);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    checkEmailVerification();
+  }, []);
   return (
     <>
       <h1>Welcome to Expense Tracker</h1>
       <p>Your Profile is incoplete </p>
       <Link to="/update_profile">Complete Now</Link>
+      <p>Your email is {emailVerified ? "verified" : "not verified"}</p>
+      <Button variant="primary" onClick={sendEmailVerification}>
+        Verify Email
+      </Button>
     </>
   );
 }
